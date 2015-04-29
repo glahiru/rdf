02dbf7013464706575b268b8ec9f36e166130c75
Lahiru Gunathilake <glahiru@gmail.com>
2015-04-28 22:08:18 -0400

    more improvements to teh UI

--- rdf-api/src/main/java/com/chirp/api/ChirpAPIResource.java
+++ rdf-api/src/main/java/com/chirp/api/ChirpAPIResource.java
@@ -20,15 +20,28 @@
 */
 package com.chirp.api;
 
+import com.chirp.api.util.RDFFile;
+import com.google.gson.Gson;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
+import com.hp.hpl.jena.util.FileManager;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Path("/")
 public class ChirpAPIResource {
@@ -36,15 +49,82 @@ public class ChirpAPIResource {
 
     public static final String CHIRP_API_VERSION = "0.1-SNAPSHOT";
 
+    private ObjectMapper objectMapper;
+
+    private List<RDFFile> rdfFiles;
+
+    private Map<String,String> rdfMap;
+
+    private final String basePath = "/Users/lginnali/masters/data-semantics/project/rdf/rdf-files";
 
     public ChirpAPIResource() throws Exception {
+        this.objectMapper = new ObjectMapper();
+        rdfFiles = new ArrayList<RDFFile>();
+        rdfMap = new HashMap<String, String>();
+
+        rdfFiles.add(new RDFFile("camera.rdf", "/Users/lginnali/masters/data-semantics/project/rdf/rdf-files/camerainfo.rdf"));
+        rdfMap.put("camera.rdf", "/Users/lginnali/masters/data-semantics/project/rdf/rdf-files/camerainfo.rdf");
+    }
+
+    @POST
+    @Path("/login")
+    public Response login(InputStream incomingData){
+        try {
+            HashMap<String,String> parameters = objectMapper.readValue(getInputString(incomingData),
+                    HashMap.class);
+            String username = parameters.get("username");
+            String password = parameters.get("password");
+            if(username.equals(password)) {
+                if (username.equals("admin")) {
+                    return Response.status(200).entity("success").build();
+                }
+            }
+            return Response.status(200).entity("fail").build();
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            return Response.status(400).entity(e.getMessage()).build();
+        }
     }
 
     @POST
     @Path("/view")
-    public Response removePrivacySettings(InputStream incomingData){
+    public Response getRDFByName(InputStream incomingData){
         try {
-            return Response.status(200).build();
+            HashMap<String,String> parameters = objectMapper.readValue(getInputString(incomingData),
+                    HashMap.class);
+            String name = parameters.get("name");
+            String path = rdfMap.get(name);
+            // reading the file
+            String rdfContent = null;
+            String line = null;
+
+            FileInputStream fileInputStream = new FileInputStream(new File(path));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            while((line=bufferedReader.readLine())!=null){
+                rdfContent+=line;
+                rdfContent += "\n";
+            }
+            return Response.status(200).entity(rdfContent).build();
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            return Response.status(400).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/getRDFMeta")
+    public Response getRDFMetadata(InputStream incomingData){
+        try {
+            List<Map> fullPostsInformation = new ArrayList<Map>();
+
+            for (int i = 0; i < rdfFiles.size(); i++) {
+                HashMap<String, String> stringStringHashMap = new HashMap<String, String>();
+                stringStringHashMap.put("name", rdfFiles.get(i).getName());
+                stringStringHashMap.put("path", rdfFiles.get(i).getPath());
+                fullPostsInformation.add(stringStringHashMap);
+            }
+            Gson gson = new Gson();
+            return Response.status(200).entity(gson.toJson(fullPostsInformation)).build();
         } catch (Exception e) {
             logger.error(e.getMessage(),e);
             return Response.status(400).entity(e.getMessage()).build();
@@ -55,7 +135,47 @@ public class ChirpAPIResource {
     @Path("/search")
     public Response getSettings(InputStream incomingData){
         try {
-            return Response.status(200).build();
+            HashMap<String,String> parameters = objectMapper.readValue(getInputString(incomingData),
+                    HashMap.class);
+            String name = parameters.get("name");
+            String query = parameters.get("query");
+            String rules = parameters.get("rule");
+
+            String path = rdfMap.get(name);
+            if(path==null){
+                return Response.status(200).build();
+            }
+
+            // Create an empty in-memory model
+            Model model = ModelFactory.createDefaultModel(); // default model this will be changed if there is a rule
+
+
+            // use the FileManager to open the bloggers RDF graph from the filesystem
+            InputStream in = FileManager.get().open(path);
+            if (in == null) {
+                throw new IllegalArgumentException("File: " + path + " not found");
+            }
+
+            // read the RDF/XML file
+            model.read(in, "");
+            InfModel infModel = null;
+            QueryExecution qe = null;
+            if(rules!=null && !rules.isEmpty()){
+                Reasoner reasoner = new GenericRuleReasoner(Rule.parseRules(rules));
+                infModel = ModelFactory.createInfModel(reasoner, model);
+            }
+            Query queryObj = QueryFactory.create(query);
+
+            // Execute the query and obtain results
+            if(infModel!=null) {
+                 qe = QueryExecutionFactory.create(queryObj, infModel);
+            }else {
+                qe = QueryExecutionFactory.create(queryObj, model);
+            }
+            ResultSet results = qe.execSelect();
+            String s = ResultSetFormatter.asText(results);
+            s = StringEscapeUtils.escapeHtml3(s);
+            return Response.status(200).entity(s).build();
         } catch (Exception e) {
             logger.error(e.getMessage(),e);
             return Response.status(400).entity(e.getMessage()).build();
@@ -77,4 +197,8 @@ public class ChirpAPIResource {
         }
         return stringBuilder.toString();
     }
+
+
+
+
 }
